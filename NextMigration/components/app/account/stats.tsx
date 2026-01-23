abda1eef938fa6ac0957d2342e89af73ba101cc2
CharlesDegraeuwe <charles.degraeuwe@student.hogent.be>
2026-01-24 00:58:36 +0100

    account added

--- NextMigration/components/app/account/stats.tsx
+++ NextMigration/components/app/account/stats.tsx
@@ -1,7 +1,18 @@
+import {useTranslations} from "next-intl";
+
 export default function Stats() {
+    const t = useTranslations("account.stats");
     return(<div className={"w-full flex flex-row gap-5"}>
-            <div className={"w-1/3 border border-studoborder/30 h-20 rounded-3xl bg-gray-700 px-10 py-5"}></div>
-            <div className={"w-1/3 border border-studoborder/30 h-20 rounded-3xl bg-gray-700 px-10 py-5"}></div>
-            <div className={"w-1/3 border border-studoborder/30 h-20 rounded-3xl bg-gray-700 px-10 py-5"}></div>
-    </div>)
+            <div className={"w-1/3 border border-studoborder/30 h-20 rounded-3xl bg-gray-700 px-10 py-5 flex items-center justify-center dark:text-white textstudodarkblue font-bold"}>
+                <span>{t("total_cards")}:</span>
+            </div>
+
+            <div className={"w-1/3 border border-studoborder/30 h-20 rounded-3xl bg-gray-700 px-10 py-5 flex items-center justify-center dark:text-white textstudodarkblue font-bold"}>
+                <span>{t("time_studied")}:</span>
+            </div>
+
+            <div className={"w-1/3 border border-studoborder/30 h-20 rounded-3xl bg-gray-700 px-10 py-5 flex items-center justify-center dark:text-white textstudodarkblue font-bold"}>
+                <span>{t("cards_mastered")}:</span>
+            </div>
+        </div>)
 }
