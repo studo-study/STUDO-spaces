b14344ca6ca47dd386d94d36f6074172935251cb
CharlesDegraeuwe <charles.degraeuwe@student.hogent.be>
2026-01-24 00:58:36 +0100

    account added

--- NextMigration/app/[locale]/(app)/account/page.tsx
+++ NextMigration/app/[locale]/(app)/account/page.tsx
@@ -1,6 +1,7 @@
 import {Metadata} from "next";
 import AccountHeader from "@/components/app/account/header";
 import Stats from "@/components/app/account/stats";
+import AccountGrid from "@/components/app/account/grid";
 
 
 export const metadata:Metadata = {
@@ -8,8 +9,9 @@ export const metadata:Metadata = {
 }
 
 export default function Page() {
-    return (<div className={"w-full px-50 flex flex-col gap-10 py-20"}>
+    return (<div className={"w-full px-50 flex flex-col gap-10 py-20 scroll-hidden"}>
         <AccountHeader/>
         <Stats/>
+        <AccountGrid/>
     </div>);
 }
