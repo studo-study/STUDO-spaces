e1442d54f6e485921a6fb701504eec89e1a58f45
CharlesDegraeuwe <charles.degraeuwe@student.hogent.be>
2026-02-02 04:12:22 +0100

    classroom page gestart

--- NextMigration/app/[locale]/(dashboard)/layout.tsx
+++ NextMigration/app/[locale]/(dashboard)/layout.tsx
@@ -1,12 +1,9 @@
-
 import {ReactNode} from "react";
-import LandingHeader from "@/components/marketing/landing_header/header";
-import LandingFooter from "@/components/marketing/landing_footer/footer";
-import DashboardHeader from "@/components/dashboard/dashboard_header/header";
+import AdminHeader from "@/components/dashboard/adminheader";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
     return (<div className={"flex flex-row w-screen bg-gray-950/50 h-screen"}>
-        <DashboardHeader />
+        <AdminHeader />
         <main className={"w-full max-h-screen overflow-y-scroll bg-gray-950/50"}>{children}</main>
     </div>);
 }
