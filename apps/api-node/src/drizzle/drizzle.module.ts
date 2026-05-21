937c2500c599ad1c099313bed7ebdd9c4f6ae5d9
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-05-21 23:51:28 +0200

    fix: migrations

--- apps/api-node/src/drizzle/drizzle.module.ts
+++ apps/api-node/src/drizzle/drizzle.module.ts
@@ -22,13 +22,7 @@ export class DrizzleModule implements OnModuleDestroy, OnModuleInit {
 
     try {
       await migrate(this.db, {
-        migrationsFolder: path.resolve(
-          __dirname,
-          '..',
-          '..',
-          '..',
-          'migrations',
-        ),
+        migrationsFolder: path.resolve(__dirname, '..', '..', 'migrations'),
       });
       this.logger.log('✅ Migrations completed!');
     } catch (error) {
