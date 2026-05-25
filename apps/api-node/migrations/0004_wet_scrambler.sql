77d8560c126e60febe57f81b675ff590840c1f5a
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-05-25 19:09:16 +0200

    fix: migration issues

--- apps/api-node/migrations/0004_wet_scrambler.sql
+++ apps/api-node/migrations/0004_wet_scrambler.sql
@@ -1,4 +1,4 @@
 ALTER TABLE "sessioncards" DROP CONSTRAINT "sessioncards_card_id_cards_id_fk";
 --> statement-breakpoint
-ALTER TABLE "cards" ADD COLUMN "term_is_latex" boolean NOT NULL;--> statement-breakpoint
+ALTER TABLE "cards" ADD COLUMN "term_is_latex" boolean NOT NULL DEFAULT false;--> statement-breakpoint
 ALTER TABLE "sessioncards" ADD CONSTRAINT "sessioncards_card_id_cards_id_fk" FOREIGN KEY ("card_id") REFERENCES "public"."cards"("id") ON DELETE cascade ON UPDATE no action;
