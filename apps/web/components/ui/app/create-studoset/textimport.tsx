d5e997d8737725c2ea512e75a8c172a5510a30c2
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-06-05 02:04:05 +0200

    feat: textimport

--- apps/web/components/ui/app/create-studoset/textimport.tsx
+++ apps/web/components/ui/app/create-studoset/textimport.tsx
@@ -1,10 +1,11 @@
 import { useTranslations } from "next-intl";
+import BaseButton from "@/components/ui/design_system/button/BaseButton";
 
 export default function Textimport() {
   const t = useTranslations("import");
   return (
     <div className="w-full h-full relative min-h-[80vh] flex flex-col items-center justify-center gap-6 px-4 py-10">
-      <div className={"w-full h-full grid grid-cols-2 gap-5 "}>
+      <div className={"w-full h-full grid grid-cols-2 gap-10 "}>
         <div className={"w-full h-full flex flex-col gap-5"}>
           <div
             className={
@@ -13,23 +14,23 @@ export default function Textimport() {
           >
             <textarea
               placeholder="paste your text here"
-              className="w-full h-full min-h-full dark:text-white resize-none p-5"
+              className="w-full h-full min-h-full dark:text-white resize-none p-5 bg-studogrey/30 scroll-hidden"
               name=""
               id=""
             ></textarea>
           </div>
           <div>
-            <button
-              type="button"
-              className={`w-full bg-w-full z-10 h-12 uppercase flex rounded-full items-center justify-center bg-gray-700 text-white cursor-not-allowed`}
-            >
-              {t("import")}
-            </button>
+            <BaseButton
+              variant={"submit"}
+              type={"button"}
+              className={"min-w-full"}
+              label={t("import")}
+            />
           </div>
         </div>
-        <div
-          className={"w-full h-full rounded-4xl border border-studoborder"}
-        ></div>
+        <div className={"w-full h-full rounded-4xl"}>
+          <span>{t("text_import_title")}</span>
+        </div>
       </div>
       <div></div>
     </div>
