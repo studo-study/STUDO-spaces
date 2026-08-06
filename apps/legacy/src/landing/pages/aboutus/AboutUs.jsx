42fd31fdafed002cfb19dfc9b2bbc8d0a8e89476
Charles <charles.degraeuwe@icloud.com>
2026-08-06 15:14:59 +0200

    chore: delete legacy (#207)

--- apps/legacy/src/landing/pages/aboutus/AboutUs.jsx
+++
@@ -1,8 +0,0 @@
-export default function AboutUs() {
-  return (
-    <div
-      className={`w-full dark:text-white text-studodarkblue 
-     max-h-screen min-h-[90vh] pt-25 p-10 md:p-20 xl:p-0 xl:pt-0 h-screen flex justify-center items-center `}
-    ></div>
-  );
-}
--- apps/legacy/src/pages/search/sets/Sets.jsx
+++
@@ -1,3 +0,0 @@
-export default function SetsPage() {
-  return <div></div>;
-}
